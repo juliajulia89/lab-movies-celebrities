5f7ea3107c12e512656c5d045b2a004e36045710
juliajulia89 <julkajanicka@gmail.com>
2021-11-16 10:57:00 +0100

    done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,34 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const express = require("express");
 
-// all your routes here
+const Celebrity = require("./../models/Celebrity.model");
 
+router.get("/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
+});
+
+router.post("/create", async (req, res) => {
+  try {
+    const { name, occupation, catchPhrase } = req.body;
+    const createdCeleb = await Celebrity.create({
+      name,
+      occupation,
+      catchPhrase,
+    });
+    console.log(createdCeleb);
+    res.redirect("/celebrities");
+  } catch (error) {
+    console.log(error);
+    res.render("celebrities/new-celebrity");
+  }
+});
+
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("celebrities/celebrities", { celebrities: celebrities });
+    })
+    .catch((error) => res.render("error", { error: error }));
+});
 module.exports = router;
