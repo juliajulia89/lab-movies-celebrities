168978818ef44ad164879b196960eaea0bec633b
juliajulia89 <julkajanicka@gmail.com>
2021-11-16 09:39:41 +0100

    done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,11 @@
 //  Add your code here
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema({
+  name: { type: String },
+  occupation: { type: String },
+  catchPhrase: { type: String },
+});
+
+module.exports = mongoose.model("Celebritiy", celebritySchema);
